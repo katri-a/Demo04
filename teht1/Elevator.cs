415b94a69db44c4f7098583c6d6be538bf57f628
k2263 <k2263@OHJE-330-WS-33>
2017-02-02 11:04:25 +0200

    teht1 done

--- teht1/Elevator.cs
+++ teht1/Elevator.cs
@@ -8,5 +8,26 @@ namespace teht1
 {
     class Elevator
     {
+
+        private int maxFloor = 5;
+        private int minFloor = 1;
+        private int floor;
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value >= minFloor && value <= maxFloor) floor = value;
+              //  else if (value == 0)
+              //      Console.WriteLine("Stop");
+                else
+                {
+                    Console.WriteLine("Not a valid floor request");
+                }
+            }
+        }
     }
 }
--- teht1/Program.cs
+++ teht1/Program.cs
@@ -10,6 +10,16 @@ namespace teht1
     {
         static void Main(string[] args)
         {
+            Elevator elevator = new Elevator();
+            int luku;
+            do
+            {
+                Console.Write("Give a floor number (1-5) > ");
+                luku = int.Parse(Console.ReadLine());
+                elevator.Floor = luku;
+
+                Console.WriteLine("Elevator is now in floor: " + elevator.Floor);
+            } while (luku != 0);
         }
     }
 }
